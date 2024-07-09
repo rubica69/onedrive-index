259b06390eb4544aa56321497388a976ba2d03e9
rubica69 <110527711+rubica69@users.noreply.github.com>
2024-07-10 05:00:10 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -20,7 +20,7 @@ module.exports = {
   title: "Yep Cock2",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: 'tmatz1/MMD',
+  baseDirectory: 'tmatz1',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -43,8 +43,8 @@ module.exports = {
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: [
-  "/Transfered",
-   "/Hani"
+  "/MMD",
+   "/Random"
 ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
